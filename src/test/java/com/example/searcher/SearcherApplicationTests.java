adcffd6656c61e4a0760f5f7a57e800c1ca7a9a9
Izual <mtsoy1990@gmail.com>
2018-10-10 21:43:04 +0300

    update java

---
+++ src/test/java/com/example/searcher/SearcherApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.searcher;
+
+import com.searcher.SearcherApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SearcherApplication.class)
+public class SearcherApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
